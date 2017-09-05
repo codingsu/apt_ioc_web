5c76131fde7544d212b1eccf78e07e8f9399a4f8
sujunwei <sjwv587@gmail.com>
2017-09-05 16:38:46 +0800

    测试

--- apt/config.py
+++ apt/config.py
@@ -5,5 +5,5 @@ dbusername = 'root'
 dbpassword = 'root'
 dbname = 'apt_ioc_pro'
 
-# rootpath = '/home/ubuntu/apt_ioc_web'
-rootpath = '/Users/sujunwei/MyWork/PycharmProjects/apt_ioc_web'
+rootpath = '/home/ubuntu/apt_ioc_web'
+# rootpath = '/Users/sujunwei/MyWork/PycharmProjects/apt_ioc_web'
